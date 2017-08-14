3359868d0fbc855e51f0704986f2a514757530e6
TheChyz <cmichal@hotmail.ca>
2017-08-13 23:44:13 -0700

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Vanesco-SC2
+# VanescoSC2
 Starcraft 2 AI: Using s2client-proto provided by Blizzard
+
+# Current Version
+VanescoSC2 - Version 0.0.1
+
+# Versions
+Version 0.0.1 - Nothing is expected to work (initial code start)
---
+++ VanescoSC2/SC2Initialization.py
@@ -0,0 +1,110 @@
+#MIT License
+#
+#Copyright (c) 2017 TheChyz
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from s2clientprotocol import sc2api_pb2
+
+import websocket
+import subprocess
+import sys
+import socket
+import time
+
+import logging
+log = logging.getLogger(__name__)
+
+def SC2Connection():
+    port = 5000
+    args = [
+        "C:\Users\chyziak\Desktop\my_folder\my_games\sc2\StarCraft II\Versions\Base55958\SC2_x64.exe", #path where SC2 x64 executable resides
+        "-listen", "127.0.0.1", #listen to localhost (Blizzard recommends)
+        "-port", str(port), #Blizzard recommends using port 5000
+        "-displayMode", "0", #"0" means viewing in windowed mode
+    ]
+    log.info("Attempting to start SC2_x64.exe")
+    sc2_process = subprocess.Popen(args, cwd="C:\Users\chyziak\Desktop\my_folder\my_games\sc2\StarCraft II\Support64")
+    log.info("Started SC2_x64.exe")
+    sc2_socket = connectSocket(port)
+    log.info("Successfully connected to sc2api")
+    return sc2_socket
+
+
+def SC2Start(sc2_socket):
+    sc2_socket.send(createGame().SerializeToString())
+    response = sc2api_pb2.Response()
+    response_bytes = sc2_socket.recv()
+    response.ParseFromString(response_bytes)
+
+    sc2_socket.send(joinGame().SerializeToString())
+    response = sc2api_pb2.Response()
+    response_bytes = sc2_socket.recv()
+    response.ParseFromString(response_bytes)
+
+    return response
+
+
+def getObservation(sc2_socket):
+    request = sc2api_pb2.Request()
+    request.observation.SetInParent()
+
+    sc2_socket.send(request.SerializeToString())
+    response = sc2api_pb2.Response()
+    response_bytes = sc2_socket.recv()
+    response.ParseFromString(response_bytes)
+
+    return response
+
+
+def createGame():
+    request = sc2api_pb2.Request()
+    request.create_game.battlenet_map_name = "Ohana LE"
+    request.create_game.disable_fog = True
+    request.create_game.realtime = True
+
+    agent = request.create_game.player_setup.add()
+    agent.type = sc2api_pb2.Participant
+    agent.race = sc2api_pb2.Terran
+
+    enemy = request.create_game.player_setup.add()
+    enemy.type = sc2api_pb2.Computer
+    enemy.race = sc2api_pb2.Random
+    enemy.difficulty = sc2api_pb2.VeryEasy
+    return request
+
+def joinGame():
+	request = sc2api_pb2.Request()
+	request.join_game.race = sc2api_pb2.Protoss
+	request.join_game.options.raw = True
+	return request
+
+
+#Try to connect to the sc2api websocket for 1 minute
+def connectSocket(port):
+    log.info("Attempting to connect to sc2api")
+    minute = 60 #number of seconds in a minute
+    for i in range(minute):
+        try:
+            log.info("Attempt #%d to connect to sc2api" % (i+1))
+            return websocket.create_connection("ws://127.0.0.1:%s/sc2api" % port, timeout=minute)
+        except socket.error:
+            log.info("Waiting 1 second before trying again")
+            time.sleep(1)
+    log.error("Could not connect to sc2api")
